49c6f25b379571cc2013301d22103eb02a1d5fb0
Federico Jure <fjure@atixlabs.com>
2021-09-21 23:42:32 -0300

    add sort wallets

--- frontend/src/sections/WalletSection.tsx
+++ frontend/src/sections/WalletSection.tsx
@@ -1,7 +1,7 @@
-import { useEffect } from "react"
+import React, { useEffect, useState } from "react"
 import { connect } from "react-redux"
 import { Dispatch } from "redux"
-import { Button, Form, Grid, Icon, List } from "semantic-ui-react"
+import { Button, Dropdown, Label, Form, Grid, Icon, List } from "semantic-ui-react"
 import { WalletCard } from "../components/WalletCard"
 import { UserWallet } from "../modules/wallet/UserWallet"
 import { loadWallets, setFavorite, setSelected } from "../modules/wallet/wallet.actions"
@@ -29,8 +29,18 @@ interface Payload {
     setSelected: (address: string) => void
 }
 
+const tagOptions = [
+    {
+        key: 'Favorites',
+        text: 'Favorites',
+        value: 'Favorites',
+        label: { color: 'orange', empty: true, circular: true },
+    }
+]
+
 export const WalletSection = connect(mapStateToProps, mapDispatchToProps)(
     ({ wallets, loadWallets, setFavorite, setSelected }: Payload) => {
+        const [order, setOrder] = useState<string | null>(null)
 
         useEffect(() => {
             loadWallets()
@@ -51,23 +61,37 @@ export const WalletSection = connect(mapStateToProps, mapDispatchToProps)(
                                 </Button>
                             </Form.Field>
                         </Form>
+
+
                     </Grid.Column>
                 </Grid.Row>
                 <Grid.Row>
+
                     <Grid.Column>
+                        <Grid.Row >
+                            <Label icon style={{ justifyContent: "center" }}>
+
+                                <header>Order by...<Icon name='long arrow alternate down'></Icon></header>
+                                <Dropdown selection clearable options={tagOptions} onChange={(e, v) => {
+                                    setOrder(v.value ? v.value as string : null)
+                                }} />
+                            </Label>
+
+
+                        </Grid.Row>
                         <List selection  >
-                            {wallets.map(wallet =>
-                                <List.Item active={wallet.selected} onClick={() => setSelected(wallet.address)}>
-                                    <List.Icon name='ethereum' size='large' verticalAlign='middle' />
-                                    <List.Content >
-                                        <WalletCard
-                                            address={wallet.address}
-                                            favorite={wallet.favorite}
-                                            old={wallet.old}
-                                            onSetFavorite={() => { setFavorite(wallet.address, !wallet.favorite) }} />
-                                    </List.Content>
-                                </List.Item>
-                            )}
+                                {(order ? [...wallets].sort((w1, w2) => w1.favorite ? -1 : 1) : [...wallets].sort()).map(wallet =>
+                                    <List.Item key={wallet.address} active={wallet.selected} onClick={() => setSelected(wallet.address)}>
+                                        <List.Icon name='ethereum' size='large' verticalAlign='middle' />
+                                        <List.Content >
+                                            <WalletCard
+                                                address={wallet.address}
+                                                favorite={wallet.favorite}
+                                                old={wallet.old}
+                                                onSetFavorite={() => { setFavorite(wallet.address, !wallet.favorite) }} />
+                                        </List.Content>
+                                    </List.Item>
+                                )}
                         </List>
                     </Grid.Column>
                 </Grid.Row>
